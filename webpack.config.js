cfbd122c52abec89a701b797d0d7169a570c4b00
callie <2541474139@qq.com>
2022-05-26 11:08:52 +0800

    update

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,27 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 
 module.exports = {
-  mode: 'production',
+  mode: 'development',
   entry: './src/index.js',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: '[name].[contenthash].js',
+  devServer: {
+    static: './dist',
   },
+  output: {
+    filename: '[name].[contenthash].js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  plugins:[new HtmlWebpackPlugin({
+    title: 'webpack_demo', 
+    template: 'src/assets/index.html', //以引号中的文件为模板生成dist文件中的html文件
+  })],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  }
 };
