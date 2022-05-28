e0647ac6e2539699f180e65619248e6fbe191cb4
callie <2541474139@qq.com>
2022-05-28 19:41:06 +0800

    update

--- src/index.js
+++ src/index.js
@@ -2,14 +2,15 @@ import './x.scss'
 import './j.less'
 import './z.styl'
 
-import png from './assets/4.png'
-console.log(png)
+// import png from './assets/4.png'
+// console.log(png)
+
+// div.innerHTML = `
+//     <img src="${png}">
+// `
+
 
 const div = document.getElementById('app1')
-div.innerHTML = `
-    <img src="${png}">
-`
-
 const button = document.createElement('button')
 button.innerText = "懒加载"
 div.appendChild(button)
